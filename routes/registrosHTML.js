b07df604eb49e03302deedca2c717e64530d1259
ahtrujillo86 <ahtrujillo86@gmail.com>
2025-11-04 23:11:56 -0600

    scale device

--- routes/registrosHTML.js
+++ routes/registrosHTML.js
@@ -19,6 +19,7 @@ router.get('/', async (req, res) => {
       <html lang="es">
       <head>
         <meta charset="UTF-8">
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <title>Lista de Registros</title>
         <style>
           body { font-family: Arial, sans-serif; margin: 40px; background-color: #f4f4f4; }
