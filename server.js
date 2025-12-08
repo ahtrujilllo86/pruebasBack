aad5fe264c1ee49a4b1c2e15716cb977bb8adad8
ahtrujillo86 <ahtrujillo86@gmail.com>
2025-12-08 13:40:04 -0600

    only validation route

--- server.js
+++ server.js
@@ -2,30 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const path = require("path");
-const fs = require("fs");
 
-
-const usuariosRoutes = require('./routes/usuarios');
-const registrosRoutes = require('./routes/registros');
-const consultaRoute = require('./routes/consulta');
-const registrosHTML = require('./routes/registrosHTML');
 const alertzy = require('./routes/alertzy');
 
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/usuarios', usuariosRoutes);
-app.use('/api/registros', registrosRoutes);
-app.use('/registros', registrosHTML);
-app.use('/consulta', consultaRoute);
 app.use('/validations', alertzy);
-
-
-app.get('/', (req, res) => {
-  res.json({ mensaje: 'API con MySQL funcionando 🚀' });
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
